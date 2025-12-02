931e5cb03b513aced5d553c4179e3751971cac1d
Filip Szablowski L4 <szablowski.zero@gmail.com>
2025-12-02 13:49:02 +0100

    Update test_hello.py

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -2,7 +2,7 @@ from fastapi.testclient import TestClient
 
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.join(__file__,"../"))))
+sys.path.append(os.path.dirname(os.path.join(__file__,"../")))
 
 def test_hello():
     client = TestClient(app)
